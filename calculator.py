c549cba9094d1af789fc1ed6b7e666e4657c9e82
Curtis Mattoon <cmattoon@cmattoon.com>
2015-11-24 03:57:04 -0500

    stuff

---
+++ calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+
+"""
+def get_prismatic_coef(V, S, L):
+    """
+        V - Volume
+        L - Length Overall
+        S - Cross-sectional area distribution
+    """
+    return V / (S x L)
