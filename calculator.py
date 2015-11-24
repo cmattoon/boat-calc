7641d3877507227373d0479020817ef58c93567a
Curtis Mattoon <cmattoon@cmattoon.com>
2015-11-24 04:30:52 -0500

    moar

--- calculator.py
+++ calculator.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
+from boatstuff.formulae import *
+from boatstuff.funcs import *
+imp = {
+    'volume': 1800,
+    'beam': 18,
+    'length': {
+        'overall': 80
+        },
+    'sa': 50
+    }
 """
+This dict will have the measurements in metric.
+"""
+metric = to_metric(imp)
+
 
 """
-def get_prismatic_coef(V, S, L):
-    """
-        V - Volume
-        L - Length Overall
-        S - Cross-sectional area distribution
-    """
-    return V / (S x L)
+Set `pc` to the result of `prismatic_coef(V, S, L)`
+"""
+pc = prismatic_coef(metric['volume'], metric['sa'], metric['length']['overall'])
+
+print("Prismatic coef is: %s" % (str(green(pc))))
